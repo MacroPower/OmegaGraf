d714cc0c895278d48e7ff6a3bc555ba30675de65
Jacob Colvin <jacobcolvin1@gmail.com>
2019-10-15 18:03:43 -0400

    Create prometheus config

---
+++ compose/src/config/Prometheus.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using SharpYaml;
+using SharpYaml.Serialization;
+
+namespace OmegaGraf.Compose.Config
+{
+    /// <summary>
+    /// Represents the prometheus.yaml config file
+    /// </summary>
+    public class Prometheus
+    {
+        [YamlMember("global")]
+        public Global Global { get; set; }
+
+        [YamlMember("scrape_configs")]
+        public List<ScrapeConfigs> ScrapeConfigs { get; set; }
+    }
+
+    public class Global
+    {
+        [YamlMember("scrape_interval")]
+        public string ScrapeInterval { get; set; }
+    }
+
+    public class ScrapeConfigs
+    {
+        [YamlMember("job_name")]
+        public string JobName { get; set; }
+
+        [YamlMember("scrape_interval")]
+        public string ScrapeInterval { get; set; }
+
+        [YamlMember("static_configs")]
+        public List<StaticConfigs> StaticConfigs { get; set; }
+    }
+
+    public class StaticConfigs
+    {
+        [YamlMember("targets")]
+        [YamlStyle(YamlStyle.Flow)]
+        public string[] Targets { get; set; }
+    }
+}
