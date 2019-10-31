787dd2385a503506864fcd41fc2bfce09dcfcc0a
Jacob Colvin <jacobcolvin1@gmail.com>
2019-10-31 19:42:58 -0400

    Only deploy VCSim as a prereq for Telegraf

--- compose-test/Setup.cs
+++ compose-test/Setup.cs
@@ -19,44 +19,5 @@ namespace OmegaGraf.Compose.Tests.Builder
                 TestContext.Out.WriteLine("Network already exists.");
             }
         }
-
-        [OneTimeSetUp]
-        public void DeployVCSim()
-        {
-            var docker = new Docker();
-            var sims = docker.ListContainers().Result.Where(x => x.Names.Contains("/og-vcsim"));
-            
-            foreach (var sim in sims)
-            {
-                docker.StopContainer(sim.ID).Wait();
-                docker.RemoveContainer(sim.ID).Wait();
-            }
-
-            var runner = new Runner();
-
-            var uuid = runner.Build(
-                new BuildConfiguration()
-                {
-                    Image = "macropower/vcsim",
-                    Tag = "latest",
-                    Ports = new List<int>(){ 8989 },
-                    Binds = new Dictionary<string, string>(){ },
-                    Parameters = new List<string>()
-                    {
-                        "--clusters", "2",
-                        "--data-centers", "1",
-                        "--data-stores", "2",
-                        "--hosts", "5",
-                        "--resource-pools", "1",
-                        "--standalone-host", "0",
-                        "--virtual-machines", "20",
-                    }
-                });
-
-            TestContext.Out.WriteLine("docker container logs " + uuid);
-            TestContext.Out.WriteLine("docker container inspect " + uuid);
-            TestContext.Out.WriteLine("docker container stop " + uuid);
-            TestContext.Out.WriteLine("docker container rm " + uuid);
-        }
     }
 }
