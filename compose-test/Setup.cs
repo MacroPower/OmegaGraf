47ca54f70fc7599c50a00d122bc658bd88f6e503
Jacob Colvin <jacobcolvin1@gmail.com>
2019-11-05 19:40:29 -0500

    Kill containers in test TearDown

--- compose-test/Setup.cs
+++ compose-test/Setup.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using OmegaGraf.Compose.MetaData;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace OmegaGraf.Compose.Tests.Builder
 {
@@ -18,6 +19,36 @@ namespace OmegaGraf.Compose.Tests.Builder
             {
                 TestContext.Out.WriteLine("Network already exists.");
             }
+        }
+
+        [OneTimeTearDown]
+        public void KillContainers()
+        {
+            var docker = new Docker();
+            var containers = docker.ListContainers().Result;
+            var tasks = new List<Task>();
+
+            foreach (var container in containers)
+            {
+                foreach (var name in container.Names)
+                {
+                    if (name.StartsWith("/og-"))
+                    {
+                        TestContext.Out
+                            .WriteLine("Removing " + name + ", " + container.ID);
+
+                        tasks.Add(
+                            Task.Run(async () => {
+                                     await docker.StopContainer(container.ID);
+                                     await docker.RemoveContainer(container.ID);
+                                 }));
+                    }
+                }
+            }
+
+            Task t = Task.WhenAll(tasks);
+
+            t.Wait();
         }
     }
 }
