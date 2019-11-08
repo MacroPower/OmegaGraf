b2e4432c5320c7897d9dc1f49e48b1cf2609efa5
Jacob Colvin <jacobcolvin1@gmail.com>
2019-11-07 19:14:59 -0500

    Add VCSim card

---
+++ src/components/deployment/VCSim.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import AuthContext from "../Context";
+import { Card, ProgressBar, Button } from "react-bootstrap";
+
+function DeployVCSim() {
+    const [progress, setProgress] = useState(0);
+
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>vCenter Simulator</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Simulates the vCenter API
+          </Card.Subtitle>
+          <Card.Text>
+          <Button variant="primary" onClick={() => setProgress(progress + 1)}>Deploy</Button>
+            <ProgressBar variant="info" now={progress} />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+}
+
+export default DeployVCSim;
