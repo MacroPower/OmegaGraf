30ff9ccdbd1e78d8acc0c6a02c90e0878d374b8a
Jacob Colvin <jacobcolvin1@gmail.com>
2020-02-05 18:55:40 -0500

    Add a spinner to the current deployment item

--- src/views/Deploy.tsx
+++ src/views/Deploy.tsx
@@ -1,23 +1,57 @@
 import React, { useState } from 'react';
 import { UseGlobalSession, UseGlobalSettings } from '../components/Global';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import Steps from 'rc-steps';
 import 'rc-steps/assets/index.css';
 import 'rc-steps/assets/iconfont.css';
+import { PacmanLoader } from 'react-spinners';
 
 export default function Deploy() {
   const [globalSession, globalSessionActions] = UseGlobalSession();
   const [globalSettings, globalSettingsActions] = UseGlobalSettings();
+
+  const [steps, setSteps] = useState([
+    {
+      title: 'Gathering Info...',
+      description: 'Fetching data from the OmegaGraf server.'
+    }
+  ]);
+
+  const addStep = (title: string, description: string) => (e: any) => {
+    const newSteps = [...steps];
+    newSteps.push({
+      title: title,
+      description: description
+    });
+    setSteps(newSteps);
+  };
 
   return (
     <main role="main" className="container">
       <h2>Deploy</h2>
       <hr />
       <Container>
-      <Steps current={1}>
-        <Steps.Step title="first" />
-        <Steps.Step title="second" />
-        <Steps.Step title="third" />
+        <Button variant="outline-primary" onClick={addStep('Test', 'Test')}>
+          +
+        </Button>
+        <Steps current={steps.length - 1} direction="vertical" size="large">
+          {steps.map((step, i) => {
+            const icon = (
+              <PacmanLoader
+                size={15}
+                color={'#007bff'}
+                loading={true}
+              />
+            );
+
+            return (
+              <Steps.Step
+                key={i}
+                {...step}
+                {...(i === steps.length - 1 && { icon: { ...icon }, style: { verticalAlign: '-.125em' } })}
+              />
+            );
+          })}
         </Steps>
       </Container>
     </main>
--- types/rc-steps/index.d.ts
+++ types/rc-steps/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'rc-steps' {
-    export default class Steps extends React.Component<{current: number}> {
-        static Step = class Step extends React.Component<{title: string}> {}
+    export default class Steps extends React.Component<{current: number, direction?: string, size?: string}> {
+        static Step = class Step extends React.Component<{title: string, description: string, icon?: any}> {}
     }
 }
