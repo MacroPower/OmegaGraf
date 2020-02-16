afc8f4a31b91d3932a2a3c86891d6445d2041317
Jacob Colvin <jacobcolvin1@gmail.com>
2020-02-15 19:56:39 -0500

    Add basic trace logging for all exceptions

--- compose/src/Startup.cs
+++ compose/src/Startup.cs
@@ -1,14 +1,18 @@
+using System;
 using Flurl.Http;
 using Flurl.Http.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Nancy.Owin;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using NLog;
 
 namespace OmegaGraf.Compose
 {
     public class Startup
     {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
         public void Configure(IApplicationBuilder app)
         {
             FlurlHttp.Configure(
@@ -21,6 +25,11 @@ namespace OmegaGraf.Compose
                     config.JsonSerializer = new NewtonsoftJsonSerializer(settings);
                 }
             );
+
+            AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
+            {
+                logger.Trace(eventArgs.Exception);
+            };
 
             app.UseOwin(x => x.UseNancy());
         }
