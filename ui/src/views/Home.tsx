1eeb6812909e8e699508b0fe8958d3319985d740
Jacob Colvin <jacobcolvin1@gmail.com>
2020-02-25 23:53:45 -0500

    UI: Add welcome title

--- ui/src/views/Home.tsx
+++ ui/src/views/Home.tsx
@@ -4,7 +4,11 @@ import DeployForm from '../components/deployment/Form';
 export default function Home() {
   return (
     <main role="main" className="container">
-      <h2>Home</h2>
+      <br />
+      <h1 className="home-title text-center">Welcome to OmegaGraf</h1>
+      <h4 className="home-subtitle text-center">Let's get started.</h4>
+      <br />
+      <hr />
       <br />
       <DeployForm />
     </main>
