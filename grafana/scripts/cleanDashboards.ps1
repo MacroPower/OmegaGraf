b7498c286542de5115c683ed08a076f6012f5478
Jacob Colvin <jacobcolvin1@gmail.com>
2020-03-16 01:42:08 -0400

    Grafana: Use prettier to format dashboards

--- grafana/scripts/cleanDashboards.ps1
+++ grafana/scripts/cleanDashboards.ps1
@@ -1,21 +1,3 @@
-function Format-Json(
-    [Parameter(Mandatory, ValueFromPipeline)][String] $json
-) {
-    $indent = 0;
-    ($json -Split "`n" | ForEach-Object {
-            if ($_ -match '[\}\]]\s*,?\s*$') {
-                # This line ends with ] or }, decrement the indentation level
-                $indent--
-            }
-            $line = ('  ' * $indent) + $($_.TrimStart() -replace '":  (["{[])', '": $1' -replace ':  ', ': ')
-            if ($_ -match '[\{\[]\s*$') {
-                # This line ends with [ or {, increment the indentation level
-                $indent++
-            }
-            $line
-        }) -Join "`n"
-}
-
 $base = '../dashboards/'
 
 (Get-ChildItem -Path $base).Name | ForEach-Object {
@@ -32,16 +14,14 @@ $base = '../dashboards/'
         $db.PSObject.properties.remove('__inputs')
         $db.PSObject.properties.remove('__requires')
 
-        $db.panels | ForEach-Object {
-            $_.datasource = $null
-        }
-
         $newFile = @{
             'dashboard' = $db
         }
 
         $json = $newFile | ConvertTo-Json -Depth 10 
 
-        Format-Json -json $json | Out-File -FilePath $filePath
+        $json | Out-File -FilePath $filePath
     }
 }
+
+prettier $base* --write
