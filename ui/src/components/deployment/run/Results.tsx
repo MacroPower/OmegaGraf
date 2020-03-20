148f8b79fc62852c221cd9b2477d8f3baece3e6d
Jacob Colvin <jacobcolvin1@gmail.com>
2020-03-20 19:46:26 -0400

    UI: Show a results table while deployment is running

---
+++ ui/src/components/deployment/run/Results.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Results(props: {
+  url: string;
+  port: string;
+  app: string;
+  message?: string;
+}) {
+  const { app, url, port, message } = props;
+  const location = `${url}:${port}`;
+  return (
+    <div className="mb-3 p-3 deploy-results">
+      <h5>{`Your new ${app} instance:`}</h5>
+      <hr />
+      <a href={location}>{location}</a>
+      {message && <hr />} {message}
+    </div>
+  );
+}
